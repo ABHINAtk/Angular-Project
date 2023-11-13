0618b22fc81ec1ef4159e973b8ea1acfcc5dac43
ABHINAtk <abhinashaji@gmail.com>
2023-11-13 18:44:17 +0530

    task-13-11-2023

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,7 +9,29 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'fe-demo-app';
-  welcome='welcome';
+
+
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    this.example();
+    
+  }
+
+example(){
+  var variable1='Hello';
+  let variable2='world';
+  const variable3='welcome';
+
+  if(2>1){
+    var variable1='goodbye';
+    let variable2="universe";
+    console.log('1',variable1);
+    console.log('2',variable2);
+    console.log('3',variable3);
+  }
+
+  console.log('4',variable1);
+  console.log('5',variable2);
+  console.log('6',variable3);
 }
+} 
